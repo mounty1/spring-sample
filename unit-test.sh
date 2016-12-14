a7d7a03615e134bb676dee8ce8473f442958b3c4
Michael Mounteney <jackrose@landcroft.com>
2016-12-14 22:03:59 +1000

    fixes to unit test script

--- unit-test.sh
+++ unit-test.sh
@@ -1,11 +1,16 @@
- set -e
- REPORT='\n>>>> status=%{response_code} type=%{content_type}\n'
- for C in a 1 2 b c d 2 3 4 e 1 f z
- do
+set -e
+REPORT='\n>>>> status=%{response_code} type=%{content_type}\n'
+for C in a 1 2 b c d 2 3 4 e 1 f z
+do
 	curl -H Accept:application/json -H Content-Type:application/json -X POST -d '{"count":"1","content":"'${C}'"}' -w "${REPORT}" http://127.0.0.1:8080/chars
 done
 curl -H Accept:application/json -H Content-Type:application/json -X GET -w "${REPORT}" http://127.0.0.1:8080/state
-curl -H Accept:application/json -H Content-Type:application/json -X GET -w "${REPORT}" http://127.0.0.1:8080/sum
-curl -H Accept:application/json -H Content-Type:application/json -X GET -w "${REPORT}" http://127.0.0.1:8080/huh
+test $(curl -H Accept:application/json -H Content-Type:application/json -X GET -s http://127.0.0.1:8080/sum | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['content'])") == "247" || echo WRONG SUM
+# test $(curl -H Accept:application/json -H Content-Type:application/json -X GET -s http://127.0.0.1:8080/huh | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['status'])") -eq 404 || echo WRONG
 curl -H Accept:application/json -H Content-Type:application/json -X DELETE -w "${REPORT}" http://127.0.0.1:8080/chars/3
-curl -H Accept:application/json -H Content-Type:application/json -X GET -w "${REPORT}" http://127.0.0.1:8080/sum
+test $(curl -H Accept:application/json -H Content-Type:application/json -X GET -s http://127.0.0.1:8080/sum | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['content'])") == "37" || echo WRONG SUM
+for C in a 1 2 b c d 2 4 e 1 f z
+do
+	curl -H Accept:application/json -H Content-Type:application/json -X DELETE -w "${REPORT}" http://127.0.0.1:8080/chars/${C}
+done
+test -z $(curl -H Accept:application/json -H Content-Type:application/json -X GET -s http://127.0.0.1:8080/state | tee ex | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['content'])") || echo NOT CLEARED
